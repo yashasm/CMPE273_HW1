38346ef3a1dd085230ac0287ad76fec47e61c049
Yashas Malleshappa <yashasm1991@gmail.com>
2016-09-06 02:29:01 -0700

    adding json

---
+++ JavaScript_Refresher/WebContent/data.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+
+var Data = '{"name":"yashas",'+
+	'"sjsuid":"010365251",'+
+	'"sem":"Third",'+
+	'"GPA" :"3.8",'+
+	'"Courses":[{"coursename":"Enterprise Distributed Systems",'+
+	'"number":"CMPE 273","professor":"Simon Shim","days":"Mon and Wed"}'+
+	',{"coursename":"Computer Network Design",'+
+	'"number":"CMPE 206","professor":"Juan Gomez","days":"Mon"}'+
+	',{"coursename":"Master Project I",'+
+	'"number":"CMPE 295A","professor":"Dan Harkey","days":"Fri"}]}';
